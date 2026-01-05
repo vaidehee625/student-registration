e01771b86a20bfd22d241c1e5039bc6845776708
Vaidehee Kumari <vaideheekumari2802@gmail.com>
2026-01-05 19:21:21 +0530

    Fix port for Render deployment

--- server.js
+++ server.js
@@ -107,6 +107,9 @@ app.get("/users", async (req, res) => {
   }
 })
 
-app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000")
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT)
 })
+
